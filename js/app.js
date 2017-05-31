16314e86a21de1fd8be80edfa9f81bc5d97800e6
chinchillapsico <coraline.colous@gmail.com>
2017-05-31 16:58:20 -0400

    cambios js e imagen pc

--- js/app.js
+++ js/app.js
@@ -1,28 +1,38 @@
+(function main(){
+	var boxes = Array.from(document.getElementsByClassName("box-services")); /*accedo al div box services*/
+	var modal = document.getElementById("box-services-modal");
+	var bodyModal, close, img;
+	/* Recorro el arreglo boxes que contiene a todas las subsecciones de "servicios". Esto lo hago con la función "box" */
+	boxes.forEach(function(box){
+		box.addEventListener("click", function(){ /*Cuando el ususario hace click se ejecuta una función que crea un string vacío en el div vacío*/
+			modal.innerHTML = "";
+			bodyModal = document.createElement("div"); /*bodyModal crea un div, le da la clase modal-body*/
+			bodyModal.classList.add("modal-body");
+			bodyModal.innerHTML = box.innerHTML;			
+			modal.appendChild(bodyModal); /*lo que está en paréntesis se hace nodo hijo, es decir bodyModal se hace nodo hijo de modal*/
+			modal.classList.remove("hide");/*esconde*/
+			close = document.createElement("div");
+			close.classList.add("close");
+			img = document.createElement("img");
+			img.setAttribute("src", "http://tojaeurope.com/gallery/Close-icon.png");
+			close.appendChild(img);
+			modal.appendChild(close);
+			close.addEventListener("click",function(){
+				modal.classList.add("hide");
+			});
+		});		
+	});
+})();
+
+	
+
 /*
- * Archivo principal de funcionalidad de JS
- */
-
- (function main(){
- 	/*función autoinvocada (function miAlert(){....
- })()   es igual a  function miAlert(){....} miAlert()*/
-
- 	var boxes = Array.from(document.getElementByClassName("box-services"));
- 	var modal = document.getElementById("box-services-modal");
- 	var bodyModal;
-
-   boxes.forEach(function(box){
-   	box.addEventListener("click", function(){
- 		modal.innerHTML = "";
- 		bodyModal = document.createElement("div");
- 		bodyModal.classList.add("modal-body");
-
- 	});
-   	
-   	
-   	bodyModal.innerHTML = box.innerHTML;
- 	 box.addEventListener("click", function(){
- 		modal.classList.remove("hide");
-
- 	});
- });
- })();
+funcion autoinvocada
+(function miAlert(){
+	....
+})()
+Es equivalente a:
+function miAlert(){
+	...
+};
+miAlert(); */
